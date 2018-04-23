be6acfe4ffa05a1a6157f06142b0d3b02fe6683e
breandan.considine <breandan.considine@gmail.com>
2018-04-23 04:57:50 -0400

    update mmd_score

--- mmd_score.py
+++ mmd_score.py
@@ -1,139 +1,171 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
 import torch
-from torch.autograd import Variable
+
+min_var_est = 1e-8
 
 
-# Adapted from http://torch-two-sample.readthedocs.io/en/latest/_modules/torch_two_sample/statistics_diff.html#MMDStatistic
-
-class MMDStatistic:
-    r"""The *unbiased* MMD test of :cite:`gretton2012kernel`.
-
-    The kernel used is equal to:
-
-    .. math ::
-        k(x, x') = \sum_{j=1}^k e^{-\alpha_j\|x - x'\|^2},
-
-    for the :math:`\alpha_j` proved in :py:meth:`~.MMDStatistic.__call__`.
-
-    Arguments
-    ---------
-    n_1: int
-        The number of points in the first sample.
-    n_2: int
-        The number of points in the second sample."""
-
-    def __init__(self, n_1, n_2):
-        self.n_1 = n_1
-        self.n_2 = n_2
-
-        # The three constants used in the test.
-        self.a00 = 1. / (n_1 * (n_1 - 1))
-        self.a11 = 1. / (n_2 * (n_2 - 1))
-        self.a01 = - 1. / (n_1 * n_2)
-
-    def __call__(self, sample_1, sample_2, alphas, ret_matrix=False):
-        r"""Evaluate the statistic.
-
-        The kernel used is
-
-        .. math::
-
-            k(x, x') = \sum_{j=1}^k e^{-\alpha_j \|x - x'\|^2},
-
-        for the provided ``alphas``.
-
-        Arguments
-        ---------
-        sample_1: :class:`torch:torch.autograd.Variable`
-            The first sample, of size ``(n_1, d)``.
-        sample_2: variable of shape (n_2, d)
-            The second sample, of size ``(n_2, d)``.
-        alphas : list of :class:`float`
-            The kernel parameters.
-        ret_matrix: bool
-            If set, the call with also return a second variable.
-
-            This variable can be then used to compute a p-value using
-            :py:meth:`~.MMDStatistic.pval`.
-
-        Returns
-        -------
-        :class:`float`
-            The test statistic.
-        :class:`torch:torch.autograd.Variable`
-            Returned only if ``ret_matrix`` was set to true."""
-        sample_12 = torch.cat((sample_1, sample_2), 0)
-        distances = pdist(sample_12, sample_12, norm=2)
-
-        kernels = None
-        for alpha in alphas:
-            kernels_a = torch.exp(- alpha * distances ** 2)
-            if kernels is None:
-                kernels = kernels_a
-            else:
-                kernels = kernels + kernels_a
-
-        k_1 = kernels[:self.n_1, :self.n_1]
-        k_2 = kernels[self.n_1:, self.n_1:]
-        k_12 = kernels[:self.n_1, self.n_1:]
-
-        mmd = (2 * self.a01 * k_12.sum() +
-               self.a00 * (k_1.sum() - torch.trace(k_1)) +
-               self.a11 * (k_2.sum() - torch.trace(k_2)))
-        if ret_matrix:
-            return mmd, kernels
-        else:
-            return mmd
-
-    def pval(self, distances, n_permutations=1000):
-        r"""Compute a p-value using a permutation test.
-
-        Arguments
-        ---------
-        matrix: :class:`torch:torch.autograd.Variable`
-            The matrix computed using :py:meth:`~.MMDStatistic.__call__`.
-        n_permutations: int
-            The number of random draws from the permutation null.
-
-        Returns
-        -------
-        float
-            The estimated p-value."""
-        if isinstance(distances, Variable):
-            distances = distances.data
-        return permutation_test_mat(distances.cpu().numpy(),
-                                    self.n_1, self.n_2,
-                                    n_permutations,
-                                    a00=self.a00, a11=self.a11, a01=self.a01)
+# Consider linear time MMD with a linear kernel:
+# K(f(x), f(y)) = f(x)^Tf(y)
+# h(z_i, z_j) = k(x_i, x_j) + k(y_i, y_j) - k(x_i, y_j) - k(x_j, y_i)
+#             = [f(x_i) - f(y_i)]^T[f(x_j) - f(y_j)]
+#
+# f_of_X: batch_size * k
+# f_of_Y: batch_size * k
+def linear_mmd2(f_of_X, f_of_Y):
+    loss = 0.0
+    delta = f_of_X - f_of_Y
+    loss = torch.mean((delta[:-1] * delta[1:]).sum(1))
+    return loss
 
 
-def pdist(sample_1, sample_2, norm=2, eps=1e-5):
-    r"""Compute the matrix of all squared pairwise distances.
-    Arguments
-    ---------
-    sample_1 : torch.Tensor or Variable
-        The first sample, should be of shape ``(n_1, d)``.
-    sample_2 : torch.Tensor or Variable
-        The second sample, should be of shape ``(n_2, d)``.
-    norm : float
-        The l_p norm to be used.
-    Returns
-    -------
-    torch.Tensor or Variable
-        Matrix of shape (n_1, n_2). The [i, j]-th entry is equal to
-        ``|| sample_1[i, :] - sample_2[j, :] ||_p``."""
-    n_1, n_2 = sample_1.size(0), sample_2.size(0)
-    norm = float(norm)
-    if norm == 2.:
-        norms_1 = torch.sum(sample_1 ** 2, dim=1, keepdim=True)
-        norms_2 = torch.sum(sample_2 ** 2, dim=1, keepdim=True)
-        norms = (norms_1.expand(n_1, n_2) +
-                 norms_2.transpose(0, 1).expand(n_1, n_2))
-        distances_squared = norms - 2 * sample_1.mm(sample_2.t())
-        return torch.sqrt(eps + torch.abs(distances_squared))
+# Consider linear time MMD with a polynomial kernel:
+# K(f(x), f(y)) = (alpha*f(x)^Tf(y) + c)^d
+# f_of_X: batch_size * k
+# f_of_Y: batch_size * k
+def poly_mmd2(f_of_X, f_of_Y, d=2, alpha=1.0, c=2.0):
+    K_XX = (alpha * (f_of_X[:-1] * f_of_X[1:]).sum(1) + c)
+    K_XX_mean = torch.mean(K_XX.pow(d))
+
+    K_YY = (alpha * (f_of_Y[:-1] * f_of_Y[1:]).sum(1) + c)
+    K_YY_mean = torch.mean(K_YY.pow(d))
+
+    K_XY = (alpha * (f_of_X[:-1] * f_of_Y[1:]).sum(1) + c)
+    K_XY_mean = torch.mean(K_XY.pow(d))
+
+    K_YX = (alpha * (f_of_Y[:-1] * f_of_X[1:]).sum(1) + c)
+    K_YX_mean = torch.mean(K_YX.pow(d))
+
+    return K_XX_mean + K_YY_mean - K_XY_mean - K_YX_mean
+
+
+def _mix_rbf_kernel(X, Y, sigma_list):
+    assert (X.size(0) == Y.size(0))
+    m = X.size(0)
+
+    Z = torch.cat((X, Y), 0)
+    ZZT = torch.mm(Z, Z.t())
+    diag_ZZT = torch.diag(ZZT).unsqueeze(1)
+    Z_norm_sqr = diag_ZZT.expand_as(ZZT)
+    exponent = Z_norm_sqr - 2 * ZZT + Z_norm_sqr.t()
+
+    K = 0.0
+    for sigma in sigma_list:
+        gamma = 1.0 / (2 * sigma ** 2)
+        K += torch.exp(-gamma * exponent)
+
+    return K[:m, :m], K[:m, m:], K[m:, m:], len(sigma_list)
+
+
+def mix_rbf_mmd2(X, Y, sigma_list, biased=True):
+    K_XX, K_XY, K_YY, d = _mix_rbf_kernel(X, Y, sigma_list)
+    # return _mmd2(K_XX, K_XY, K_YY, const_diagonal=d, biased=biased)
+    return _mmd2(K_XX, K_XY, K_YY, const_diagonal=False, biased=biased)
+
+
+def mix_rbf_mmd2_and_ratio(X, Y, sigma_list, biased=True):
+    K_XX, K_XY, K_YY, d = _mix_rbf_kernel(X, Y, sigma_list)
+    # return _mmd2_and_ratio(K_XX, K_XY, K_YY, const_diagonal=d, biased=biased)
+    return _mmd2_and_ratio(K_XX, K_XY, K_YY, const_diagonal=False, biased=biased)
+
+
+################################################################################
+# Helper functions to compute variances based on kernel matrices
+################################################################################
+
+
+def _mmd2(K_XX, K_XY, K_YY, const_diagonal=False, biased=False):
+    m = K_XX.size(0)  # assume X, Y are same shape
+
+    # Get the various sums of kernels that we'll use
+    # Kts drop the diagonal, but we don't need to compute them explicitly
+    if const_diagonal is not False:
+        diag_X = diag_Y = const_diagonal
+        sum_diag_X = sum_diag_Y = m * const_diagonal
     else:
-        dim = sample_1.size(1)
-        expanded_1 = sample_1.unsqueeze(1).expand(n_1, n_2, dim)
-        expanded_2 = sample_2.unsqueeze(0).expand(n_1, n_2, dim)
-        differences = torch.abs(expanded_1 - expanded_2) ** norm
-        inner = torch.sum(differences, dim=2, keepdim=False)
-        return (eps + inner) ** (1. / norm)
+        diag_X = torch.diag(K_XX)  # (m,)
+        diag_Y = torch.diag(K_YY)  # (m,)
+        sum_diag_X = torch.sum(diag_X)
+        sum_diag_Y = torch.sum(diag_Y)
+
+    Kt_XX_sums = K_XX.sum(dim=1) - diag_X  # \tilde{K}_XX * e = K_XX * e - diag_X
+    Kt_YY_sums = K_YY.sum(dim=1) - diag_Y  # \tilde{K}_YY * e = K_YY * e - diag_Y
+    K_XY_sums_0 = K_XY.sum(dim=0)  # K_{XY}^T * e
+
+    Kt_XX_sum = Kt_XX_sums.sum()  # e^T * \tilde{K}_XX * e
+    Kt_YY_sum = Kt_YY_sums.sum()  # e^T * \tilde{K}_YY * e
+    K_XY_sum = K_XY_sums_0.sum()  # e^T * K_{XY} * e
+
+    if biased:
+        mmd2 = ((Kt_XX_sum + sum_diag_X) / (m * m)
+                + (Kt_YY_sum + sum_diag_Y) / (m * m)
+                - 2.0 * K_XY_sum / (m * m))
+    else:
+        mmd2 = (Kt_XX_sum / (m * (m - 1))
+                + Kt_YY_sum / (m * (m - 1))
+                - 2.0 * K_XY_sum / (m * m))
+
+    return mmd2
+
+
+def _mmd2_and_ratio(K_XX, K_XY, K_YY, const_diagonal=False, biased=False):
+    mmd2, var_est = _mmd2_and_variance(K_XX, K_XY, K_YY, const_diagonal=const_diagonal, biased=biased)
+    loss = mmd2 / torch.sqrt(torch.clamp(var_est, min=min_var_est))
+    return loss, mmd2, var_est
+
+
+def _mmd2_and_variance(K_XX, K_XY, K_YY, const_diagonal=False, biased=False):
+    m = K_XX.size(0)  # assume X, Y are same shape
+
+    # Get the various sums of kernels that we'll use
+    # Kts drop the diagonal, but we don't need to compute them explicitly
+    if const_diagonal is not False:
+        diag_X = diag_Y = const_diagonal
+        sum_diag_X = sum_diag_Y = m * const_diagonal
+        sum_diag2_X = sum_diag2_Y = m * const_diagonal ** 2
+    else:
+        diag_X = torch.diag(K_XX)  # (m,)
+        diag_Y = torch.diag(K_YY)  # (m,)
+        sum_diag_X = torch.sum(diag_X)
+        sum_diag_Y = torch.sum(diag_Y)
+        sum_diag2_X = diag_X.dot(diag_X)
+        sum_diag2_Y = diag_Y.dot(diag_Y)
+
+    Kt_XX_sums = K_XX.sum(dim=1) - diag_X  # \tilde{K}_XX * e = K_XX * e - diag_X
+    Kt_YY_sums = K_YY.sum(dim=1) - diag_Y  # \tilde{K}_YY * e = K_YY * e - diag_Y
+    K_XY_sums_0 = K_XY.sum(dim=0)  # K_{XY}^T * e
+    K_XY_sums_1 = K_XY.sum(dim=1)  # K_{XY} * e
+
+    Kt_XX_sum = Kt_XX_sums.sum()  # e^T * \tilde{K}_XX * e
+    Kt_YY_sum = Kt_YY_sums.sum()  # e^T * \tilde{K}_YY * e
+    K_XY_sum = K_XY_sums_0.sum()  # e^T * K_{XY} * e
+
+    Kt_XX_2_sum = (K_XX ** 2).sum() - sum_diag2_X  # \| \tilde{K}_XX \|_F^2
+    Kt_YY_2_sum = (K_YY ** 2).sum() - sum_diag2_Y  # \| \tilde{K}_YY \|_F^2
+    K_XY_2_sum = (K_XY ** 2).sum()  # \| K_{XY} \|_F^2
+
+    if biased:
+        mmd2 = ((Kt_XX_sum + sum_diag_X) / (m * m)
+                + (Kt_YY_sum + sum_diag_Y) / (m * m)
+                - 2.0 * K_XY_sum / (m * m))
+    else:
+        mmd2 = (Kt_XX_sum / (m * (m - 1))
+                + Kt_YY_sum / (m * (m - 1))
+                - 2.0 * K_XY_sum / (m * m))
+
+    var_est = (
+            2.0 / (m ** 2 * (m - 1.0) ** 2) * (
+                2 * Kt_XX_sums.dot(Kt_XX_sums) - Kt_XX_2_sum + 2 * Kt_YY_sums.dot(Kt_YY_sums) - Kt_YY_2_sum)
+            - (4.0 * m - 6.0) / (m ** 3 * (m - 1.0) ** 3) * (Kt_XX_sum ** 2 + Kt_YY_sum ** 2)
+            + 4.0 * (m - 2.0) / (m ** 3 * (m - 1.0) ** 2) * (
+                        K_XY_sums_1.dot(K_XY_sums_1) + K_XY_sums_0.dot(K_XY_sums_0))
+            - 4.0 * (m - 3.0) / (m ** 3 * (m - 1.0) ** 2) * (K_XY_2_sum) - (8 * m - 12) / (
+                        m ** 5 * (m - 1)) * K_XY_sum ** 2
+            + 8.0 / (m ** 3 * (m - 1.0)) * (
+                    1.0 / m * (Kt_XX_sum + Kt_YY_sum) * K_XY_sum
+                    - Kt_XX_sums.dot(K_XY_sums_1)
+                    - Kt_YY_sums.dot(K_XY_sums_0))
+    )
+    return mmd2, var_est
